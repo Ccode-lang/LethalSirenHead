4d390ec573f8010dc046699021ca6ef4a9f8c8a3
Ccode-lang <coop112097@gmail.com>
2024-02-09 19:42:46 -0700

    Add project files.

---
+++ Enemy/SirenHeadAI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace LethalSirenHead.Enemy
+{
+    public abstract class SirenHeadAI : EnemyAI
+    {
+    }
+
+    public class Uitls
+    {
+        public void Spawn()
+        {
+            GameObject enemy = UnityEngine.Object.Instantiate(Plugin.Enemy);
+            enemy.AddComponent<NetworkObject>();
+            enemy.AddComponent<SirenHeadAI>();
+            ScanNodeProperties Scan = enemy.transform.Find("ScanNode").gameObject.AddComponent<ScanNodeProperties>();
+            Scan.headerText = "Siren Head";
+            Scan.subText = "RUN";
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Configuration;
+using UnityEngine;
+using LethalSirenHead.Enemy;
+
+namespace LethalSirenHead
+{
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "Ccode.SirenHead";
+        private const string PluginName = "DeathSirenHead";
+        private const string VersionString = "0.0.1";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        public static GameObject Enemy;
+        public void Awake()
+        {
+            string location = ((BaseUnityPlugin)this).Info.Location;
+            string text = "L.dll";
+            string text2 = location.TrimEnd(text.ToCharArray());
+            string text3 = text2 + "enemymodel";
+            AssetBundle val = AssetBundle.LoadFromFile(text3);
+            Enemy = val.LoadAsset<GameObject>("SirenHead");
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
