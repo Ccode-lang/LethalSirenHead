c6b1cf42e31da195cdce9925462f7a53e8293617
Ccode-lang <coop112097@gmail.com>
2024-02-10 12:12:10 -0700

    Remake for new plugin version

--- Enemy/SirenHeadAI.cs
+++ Enemy/SirenHeadAI.cs
@@ -8,20 +8,7 @@ using UnityEngine;
 
 namespace LethalSirenHead.Enemy
 {
-    public abstract class SirenHeadAI : EnemyAI
+    public class SirenHeadAI : EnemyAI
     {
-    }
-
-    public class Uitls
-    {
-        public void Spawn()
-        {
-            GameObject enemy = UnityEngine.Object.Instantiate(Plugin.Enemy);
-            enemy.AddComponent<NetworkObject>();
-            enemy.AddComponent<SirenHeadAI>();
-            ScanNodeProperties Scan = enemy.transform.Find("ScanNode").gameObject.AddComponent<ScanNodeProperties>();
-            Scan.headerText = "Siren Head";
-            Scan.subText = "RUN";
-        }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -8,32 +8,55 @@ using System.Text;
 using System.Threading.Tasks;
 using BepInEx.Configuration;
 using UnityEngine;
+using LethalLib.Modules;
+using static LethalLib.Modules.Levels;
+using static LethalLib.Modules.Enemies;
 using LethalSirenHead.Enemy;
+using System.IO;
+using System.Reflection;
 
 namespace LethalSirenHead
 {
+    [BepInPlugin(Plugin.MyGuid, Plugin.PluginName, Plugin.VersionString)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
     public class Plugin : BaseUnityPlugin
     {
         private const string MyGuid = "Ccode.SirenHead";
-        private const string PluginName = "DeathSirenHead";
+        private const string PluginName = "SirenHead";
         private const string VersionString = "0.0.1";
 
         private static readonly Harmony Harmony = new Harmony(MyGuid);
 
         public static ManualLogSource Log;
 
-        public static GameObject Enemy;
+        public static EnemyType SirenEnemy;
         public void Awake()
         {
-            string location = ((BaseUnityPlugin)this).Info.Location;
-            string text = "L.dll";
-            string text2 = location.TrimEnd(text.ToCharArray());
-            string text3 = text2 + "enemymodel";
-            AssetBundle val = AssetBundle.LoadFromFile(text3);
-            Enemy = val.LoadAsset<GameObject>("SirenHead");
+            Assets.PopulateAssets();
+            SirenEnemy = Assets.MainAssetBundle.LoadAsset<EnemyType>("SirenHead");
+            var Node = Assets.MainAssetBundle.LoadAsset<TerminalNode>("SirenHeadTN");
+            var Keyword = Assets.MainAssetBundle.LoadAsset<TerminalKeyword>("SirenHeadKW");
+            NetworkPrefabs.RegisterNetworkPrefab(SirenEnemy.enemyPrefab);
             Harmony.PatchAll();
             Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            RegisterEnemy(SirenEnemy, 100, LevelTypes.VowLevel, SpawnType.Outside, Node, Keyword);
             Log = Logger;
+        }
+    }
+
+    public static class Assets
+    {
+        public static AssetBundle MainAssetBundle = null;
+        public static void PopulateAssets()
+        {
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            MainAssetBundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "modassets"));
+            if (MainAssetBundle == null)
+            {
+                Plugin.Log.LogError("Failed to load custom assets.");
+                return;
+            }
         }
     }
 }
